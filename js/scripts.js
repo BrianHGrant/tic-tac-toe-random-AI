a357ba2e8ff8f75757fd3da720f6910f781c2b72
Brian Grant and bo zhao <brian@>
2016-06-16 14:47:28 -0700

    create hard AI using strategy

--- js/scripts.js
+++ js/scripts.js
@@ -54,7 +54,6 @@ Player.prototype.mark = function(){
 
 function Game(){
   this.turn = "X";
-  this.over = 0;
   this.turnCounter = 0;
 }
 
@@ -81,6 +80,7 @@ function fillSpace(x, y, output, players, board,game,playerX,playerO){
       $(output).html("<img src='img/X.png'>");
       game.turn = "O";
       game.turnCounter++;
+      win(board.space,x,y);
 
       if (players === 1) {
         var randOne = x;
@@ -96,26 +96,126 @@ function fillSpace(x, y, output, players, board,game,playerX,playerO){
           game.turn = "X";
           game.turnCounter++;
           }
-          win(board.space,x,y);
+          win(board.space,randOne,randTwo);
+      }
+        else if (players === 0) {
+          if(game.turnCounter === 1 && x === 0 && y === 0 || game.turnCounter === 1 && x === 0 && y === 2 || game.turnCounter === 1 && x === 2 && y === 0 || game.turnCounter === 1 && x === 2 && y === 2) {
+            board.space[1][1].spaceMark(playerO)
+            var spaceID = "1" + "1" + "output";
+            $("#" + spaceID).html("<img src='img/o.png'>");
+            game.turn = "X";
+            game.turnCounter++;
+            win(board.space,x,y);
+          }
+          else if(game.turnCounter === 1 && x === 1 && y === 1) {
+            board.space[0][2].spaceMark(playerO)
+            var spaceID = "0" + "2" + "output";
+            $("#" + spaceID).html("<img src='img/o.png'>");
+            game.turn = "X";
+            game.turnCounter++;
+            win(board.space,x,y);
+          }
+          else if(game.turnCounter === 1 && x === 1 && y === 0 || game.turnCounter === 1 && x === 0 && y === 1 || game.turnCounter === 1 && x === 2 && y === 1 || game.turnCounter === 1 && x === 1 && y === 2) {
+            board.space[1][1].spaceMark(playerO)
+            var spaceID = "1" + "1" + "output";
+            $("#" + spaceID).html("<img src='img/o.png'>");
+            game.turn = "X";
+            game.turnCounter++;
+            win(board.space,x,y);
+          }
+          else if (game.turnCounter > 1) {
+            for (i =0; i <=2; i++) {
+              for(j = 0; j <=2; j++) {
+                if(board.space[i][j].mark ==="O" && board.space[i][(j+1)%3].mark === "O" && board.space[i][(j+2)%3].mark === undefined) {
+                    board.space[i][(j+2)%3].spaceMark(playerO);
+                    var spaceID = i.toString() + ((j+2)%3).toString() + "output";
+                    $("#" + spaceID).html("<img src='img/o.png'>");
+                    game.turn = "X";
+                    game.turnCounter++;
+                    win(board.space,i,(j+2)%3);
+                    return;
+                }
+                else if(board.space[i][j].mark ==="O" && board.space[(i+1)%3][j].mark === "O" && board.space[(i+2)%3][j].mark === undefined) {
+                    board.space[(i+2)%3][j].spaceMark(playerO);
+                    var spaceID = ((i+2)%3).toString() + j.toString() + "output";
+                    $("#" + spaceID).html("<img src='img/o.png'>");
+                    game.turn = "X";
+                    game.turnCounter++;
+                    win(board.space,(i+2)%3,j);
+                    return;
+                }
+                else if(board.space[i][j].mark ==="O" && board.space[(i+1)%3][(j+1)%3].mark === "O" && board.space[(i+2)%3][(j+2)%3].mark === undefined) {
+                    board.space[(i+2)%3][(j+2)%3].spaceMark(playerO);
+                    var spaceID = ((i+2)%3).toString() + ((j+2)%3).toString() + "output";
+                    $("#" + spaceID).html("<img src='img/o.png'>");
+                    game.turn = "X";
+                    game.turnCounter++;
+                    win(board.space,(i+2)%3,(j+2)%3);
+                    return;
+                }
+                else if(board.space[i][j].mark ==="X" && board.space[i][(j+1)%3].mark === "X" && board.space[i][(j+2)%3].mark === undefined) {
+                    board.space[i][(j+2)%3].spaceMark(playerO);
+                    var spaceID = i.toString() + ((j+2)%3).toString() + "output";
+                    $("#" + spaceID).html("<img src='img/o.png'>");
+                    game.turn = "X";
+                    game.turnCounter++;
+                    win(board.space,i,(j+2)%3);
+                    return;
+                }
+                else if(board.space[i][j].mark ==="X" && board.space[(i+1)%3][j].mark === "X" && board.space[(i+2)%3][j].mark === undefined ) {
+                    board.space[(i+2)%3][j].spaceMark(playerO);
+                    var spaceID = ((i+2)%3).toString() + j.toString() + "output";
+                    $("#" + spaceID).html("<img src='img/o.png'>");
+                    game.turn = "X";
+                    game.turnCounter++;
+                    win(board.space,(i+2)%3,j);
+                    return;
+                }
+                else if(board.space[i][j].mark ==="X" && board.space[(i+1)%3][(j+1)%3].mark === "X" && board.space[(i+2)%3][(j+2)%3].mark === undefined) {
+                    board.space[(i+2)%3][(j+2)%3].spaceMark(playerO);
+                    var spaceID = ((i+2)%3).toString() + ((j+2)%3).toString() + "output";
+                    $("#" + spaceID).html("<img src='img/o.png'>");
+                    game.turn = "X";
+                    game.turnCounter++;
+                    win(board.space,(i+2)%3,(j+2)%3);
+                    return;
+                }
+                else {
+                  var randOne = x;
+                  var randTwo = y;
+                  if(game.turnCounter<9){
+                    while( board.space[randOne][randTwo].mark !== undefined){
+                      randOne = Math.floor(Math.random()*3);
+                      randTwo = Math.floor(Math.random()*3);
+                    }
+                    board.space[randOne][randTwo].spaceMark(playerO);
+                    var spaceID = randOne.toString() + randTwo.toString() + "output";
+                    $("#" + spaceID).html("<img src='img/o.png'>");
+                    game.turn = "X";
+                    game.turnCounter++;
+                    }
+                    win(board.space,randOne,randTwo);
+                    return;
+                }
+              }
+            }
+          }
         }
       }
-
       else {
         board.space[x][y].spaceMark(playerO)
         $(output).html("<img src='img/o.png'>");
         game.turn = "X";
         game.turnCounter++;
       }
-
-    win(board.space,x,y);
   }
-  else if(game.turnCounter === 9){
+  if(game.turnCounter === 9){
     gameOver("Nobody");
   }
 }
 
 function gameOver(player){
-    $("body").html("<h1>congratulations, "+ player + " won!</h1>"
+    $("#results").html("<h1>congratulations, "+ player + " won!</h1>"
                   + "<br> <button type='button' id='playAgain' class='btn btn-primary'>Play Again!</button>");
     $("#playAgain").click(function() {
       location.reload(true)
